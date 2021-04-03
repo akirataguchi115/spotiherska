dd5283d9638afa56d2ec8f392ee4e9cf663d88dd
Akira Taguchi <akirahattivatti@gmail.com>
2021-04-03 09:22:56 +0300

    init

---
+++ src/main/kotlin/com/example/spotiherska/SpotiherskaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spotiherska
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpotiherskaApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpotiherskaApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/spotiherska/SpotiherskaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.spotiherska
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpotiherskaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
